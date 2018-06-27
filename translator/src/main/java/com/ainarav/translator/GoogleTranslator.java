a0747234ef791929a03e8ae6e3baff86903f08b2
ainar <ainar@DESKTOP-FQ697VU>
2018-06-27 02:25:02 +0200

    Add unit tests and translator skeleton

--- translator/src/main/java/com/ainarav/translator/GoogleTranslator.java
+++ translator/src/main/java/com/ainarav/translator/GoogleTranslator.java
@@ -1,12 +1,10 @@
 package com.ainarav.translator;
 
 import static java.net.URLEncoder.encode;
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
+import java.io.UnsupportedEncodingException;
 import java.net.URLConnection;
 import java.util.Collection;
 
@@ -18,50 +16,40 @@ import javax.json.JsonReader;
 /**
  * Google API-based translator
  */
-public class GoogleTranslator implements Translator {
+public class GoogleTranslator extends AbstractTranslator {
 
 	private static final String URL = "https://www.googleapis.com/language/translate/v2/";
 	private static final String KEY = "AIzaSyDH-0lb3TIHBPss6zZwFLrw6vzdzN9MkXo";
-	private static final String CHARSET = UTF_8.name();
 
 	@Override
-	public String translate(String text, String sourceLang, String targetLang) {
-		URLConnection connection;
-		try {
-			// connect
-			connection = new URL(URL).openConnection();
-			connection.setDoOutput(true);
-			connection.setRequestProperty("Accept-Charset", CHARSET);
-			connection.setRequestProperty("Content-Type",
-					"application/x-www-form-urlencoded;charset=" + CHARSET);
+	protected String getUrl() {
+		return URL;
+	}
 
-			// call translate api
-			String query = String.format("key=%s&source=%s&target=%s&q=%s",
-					encode(KEY, CHARSET),
-					encode(sourceLang, CHARSET),
-					encode(targetLang, CHARSET),
-					encode(text, CHARSET));
-
-			try (OutputStream output = connection.getOutputStream()) {
-				output.write(query.getBytes(CHARSET));
-			}
-
-			// read response
-			try (InputStream response = connection.getInputStream();
-					JsonReader rdr = Json.createReader(response)) {
-				JsonObject obj = rdr.readObject();
-				JsonObject data = obj.getJsonObject("data");
-				JsonArray translationArray = data.getJsonArray("translations");
-				Collection<JsonObject> translations = translationArray.getValuesAs(JsonObject.class);
-				if (!translations.isEmpty()) {
-					return translations.iterator().next().getString("translatedText");
-				}
-			}
-
-		} catch (IOException e) {
-			throw new RuntimeException(e);
+	@Override
+	protected String buildQuery(String text, String sourceLang, String targetLang) throws UnsupportedEncodingException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("key=").append(encode(KEY, CHARSET));
+		sb.append("&q=").append(encode(text, CHARSET));
+		sb.append("&target=").append(encode(targetLang, CHARSET));
+		if (sourceLang != null && !"".equals(sourceLang.trim())) {
+			sb.append("&source=").append(encode(sourceLang, CHARSET));
 		}
+		return sb.toString();
+	}
 
+	@Override
+	protected String readResponse(URLConnection connection) throws IOException {
+		try (InputStream response = connection.getInputStream(); JsonReader rdr = Json.createReader(response)) {
+			JsonObject obj = rdr.readObject();
+			JsonObject data = obj.getJsonObject("data");
+			JsonArray translationArray = data.getJsonArray("translations");
+			Collection<JsonObject> translations = translationArray.getValuesAs(JsonObject.class);
+			if (!translations.isEmpty()) {
+				return translations.iterator().next().getString("translatedText");
+			}
+		}
 		return null;
 	}
+
 }
--- translator/src/main/java/com/ainarav/translator/Translator.java
+++ translator/src/main/java/com/ainarav/translator/Translator.java
@@ -2,6 +2,9 @@ package com.ainarav.translator;
 
 /**
  * Interface for translating a text string
+ * 
+ * @author ainar
+ *
  */
 public interface Translator {
 
@@ -9,13 +12,14 @@ public interface Translator {
 	 * Translates a text string from one language to another.
 	 *
 	 * @param text
-	 *            Required. A string representing the text to translate. The size of the text must not exceed
-	 *            10000 characters.
+	 *            Required. A string representing the text to translate. The size of
+	 *            the text must not exceed 10000 characters.
 	 * @param sourceLang
-	 *            Optional. A string representing the language code of the translation text. For example,
-	 *            <code>en</code> for English.
+	 *            Optional. A string representing the language code of the
+	 *            translation text. For example, <code>en</code> for English.
 	 * @param targetLang
-	 *            A string Required. representing the language code to translate the text into.
+	 *            A string Required. representing the language code to translate the
+	 *            text into.
 	 * @return A string representing the translated text.
 	 */
 	String translate(String text, String sourceLang, String targetLang);
